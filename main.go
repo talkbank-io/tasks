7b4b576d4adc112b35c3d6fae36c55de85b828d4
Leshanu E <e.leshanu@talkbank.io>
2018-04-13 14:57:41 +0300

    Зафиксировали работу утилиты

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ import (
 	"encoding/json"
 	"os"
 	"fmt"
+	"runtime"
+	"strings"
 
 	//"github.com/rakanalh/scheduler"
 	//"github.com/rakanalh/scheduler/storage"
 	"github.com/go-pg/pg"
 	"github.com/streadway/amqp"
 	"github.com/killer-djon/tasks/pgdb"
-	//"github.com/killer-djon/tasks/consumers"
+	"github.com/killer-djon/tasks/consumers"
 )
+
+// Parse json config file
+var amqpString = make(map[string]map[string]string)
+func init() {
+	amqpString := parseConfig("./config.json")
+	runtime.GOMAXPROCS(amqpString["maxprocs"])
+}
 
 // Read config data.json
 func parseConfig(configFile string) map[string]map[string]string {
@@ -59,83 +68,11 @@ func RunSchedulerTask(d amqp.Delivery, db *pg.DB) {
 	}
 }
 
-func readRabbitConsume(amqpURI string, queueName string) {
-	conn, err := amqp.Dial(amqpURI)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	channel, err := conn.Channel()
-
-	if err != nil {
-		log.Fatalf("Open channel: %v", err)
-	}
-	defer channel.Close()
-
-	q, err := channel.QueueDeclare(
-		queueName, // name
-		true,      // durable
-		false,     // delete when unused
-		false,     // exclusive
-		false,     // no-wait
-		nil,       // arguments
-	)
-	if err != nil {
-		log.Fatalf("Queue.declare: %v", err)
-	}
-
-	err = channel.ExchangeDeclare(
-		queueName,   // name
-		"direct", // type
-		true,     // durable
-		false,    // auto-deleted
-		false,    // internal
-		false,    // no-wait
-		nil,      // arguments
-	)
-
-	err = channel.QueueBind(q.Name, "", q.Name, false, nil)
-	if err != nil {
-		log.Fatalf("Queue.bind: %v", err)
-	}
-
-
-	deliveries, err := channel.Consume(
-		queueName, //queue name
-		"", //consumerTag
-		true, //autoAck
-		false, //exclusive
-		false, //nolocal
-		false, //noWait
-		nil)        //arguments
-
-	if err != nil {
-		log.Fatalf("basic.consume: %v", err)
-	}
-
-	// Parse json config file
-	amqpString := parseConfig("./config.json")
-
-	db := pgdb.Connect(amqpString["database"])
-
-	defer db.Close()
-
-	forever := make(chan bool)
-	go func() {
-		for d := range deliveries {
-			RunSchedulerTask(d, db)
-		}
-	}()
-
-	log.Printf(" Waiting for Messages to process. To exit press CTRL+C ")
-	<- forever
-}
 
 func main() {
-	// Parse json config file
-	amqpString := parseConfig("./config.json")
+
 	// get format string amqpURI
-	amqpUri := fmt.Sprintf("amqp://%s:%s@%s:%s%s",
+	/*amqpUri := fmt.Sprintf("amqp://%s:%s@%s:%s%s",
 		amqpString["amqp"]["user"],
 		amqpString["amqp"]["password"],
 		amqpString["amqp"]["host"],
@@ -144,4 +81,5 @@ func main() {
 
 	// start consumer
 	readRabbitConsume(amqpUri, amqpString["amqp"]["queueName"])
+	*/
 }
