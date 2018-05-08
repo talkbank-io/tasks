f430083feea02534e3aea2a59a6ca07dd1e3e193
Leshanu E <e.leshanu@talkbank.io>
2018-05-08 13:19:00 +0300

    Добавил в хеш тега консьюмера pidID

--- consumers/consumers.go
+++ consumers/consumers.go
@@ -7,6 +7,8 @@ import (
 	"time"
 	"crypto/sha256"
 	"encoding/hex"
+	"os"
+	"strconv"
 
 	"github.com/streadway/amqp"
 )
@@ -41,6 +43,7 @@ exchangeType,
 bindingKey string) *Consumer {
 	if( consumerTag == "" ) {
 		tag := sha256.New()
+		tag.Write([]byte(strconv.Itoa(os.Getpid())))
 		tag.Write([]byte(time.Now().UTC().Format("2006-01-02 15:04:05")))
 		sum := tag.Sum(nil)
 		consumerTag = "consumer_GOLANG_" + hex.EncodeToString(sum)
