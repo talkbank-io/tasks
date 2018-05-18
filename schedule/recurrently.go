4361551f98d324ca9432dd696d13b9ead8b2cb19
Leshanu E <e.leshanu@talkbank.io>
2018-05-18 19:26:32 +0300

    работы по времени

--- schedule/recurrently.go
+++ schedule/recurrently.go
@@ -32,14 +32,14 @@ func NewRecurrently(scheduleModel *model.ScheduleTask, pub *publisher.Publisher,
 func (schedule *Recurrently) Run(publisherConfig map[string]interface{}, cronJob *cron.Cron) map[string]int {
 	entry := cronJob.EntryById(schedule.row.Id)
 
-	nextRun, _ := time.Parse("2006-01-02 15:04:00", schedule.row.NextRun.UTC().Format("2006-01-02 15:04:00"))
+	nextRun, _ := time.Parse("2006-01-02 15:04:00", schedule.row.NextRun.Format("2006-01-02 15:04:00"))
 	//fromDateTime, _ := time.Parse("2006-01-02 15:04:00", schedule.row.FromDatetime.Format("2006-01-02 15:04:00"))
 	now, _ := time.Parse("2006-01-02 15:04:00", time.Now().UTC().Format("2006-01-02 15:04:00"))
 
 	if( entry != nil ) {
-		fmt.Printf("Recurrently entry to be runned: Current time=%v, Next time=%v, Now time=%v, Next runtime=%v\n",
-			entry.Prev.UTC(),
-			entry.Next.UTC(),
+		fmt.Printf("Recurrently entry to be runned: JobPrev time=%v, JobNext time=%v, Now time=%v, Next runtime=%v\n",
+			entry.Prev,
+			entry.Next,
 			now,
 			nextRun)
 	}
