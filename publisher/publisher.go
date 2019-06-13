f299833fa36ab0752ce067726fb8c20871b1549c
Pavel Loginov <ploginoff@gmail.com>
2019-06-13 18:05:33 +0300

    memory leak issue

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -31,50 +31,52 @@ func (pub *Publisher) Close() {
 * Публикация данных в очередь
 */
 func (pub *Publisher) Publish(queue_name string, message []byte) (bool, error) {
+    // memory leak defer
+    return func() bool, error {
+        fmt.Println("Queue to publish:", queue_name)
 
-	fmt.Println("Queue to publish:", queue_name)
+        c, err := pub.conn.Channel()
+        if err != nil {
+            log.Fatalf("channel.open: %v", err)
+            return false, err
+        }
 
-	c, err := pub.conn.Channel()
-	if err != nil {
-		log.Fatalf("channel.open: %v", err)
-		return false, err
-	}
+        defer c.Close()
 
-	defer c.Close()
+        // Declare queue if not exists
+        Queue, err := c.QueueDeclare(
+            queue_name,
+            true,
+            false,
+            false,
+            false,
+            amqp.Table{},
+        )
 
-	// Declare queue if not exists
-	Queue, err := c.QueueDeclare(
-		queue_name,
-		true,
-		false,
-		false,
-		false,
-		amqp.Table{},
-	)
+        if err != nil {
+            log.Fatalf("Declare queue interrupt: %v", err)
+            return false, err
+        }
 
-	if err != nil {
-		log.Fatalf("Declare queue interrupt: %v", err)
-		return false, err
-	}
+        // Publish message to queue
+        err = c.Publish(
+            "",
+            Queue.Name,
+            false,
+            false,
+            amqp.Publishing{
+                DeliveryMode: amqp.Persistent,
+                Headers:         amqp.Table{},
+                ContentType:     "application/json",
+                Body:         message,
+            },
+        )
 
-	// Publish message to queue
-	err = c.Publish(
-		"",
-		Queue.Name,
-		false,
-		false,
-		amqp.Publishing{
-			DeliveryMode: amqp.Persistent,
-			Headers:         amqp.Table{},
-			ContentType:     "application/json",
-			Body:         message,
-		},
-	)
+        if err != nil {
+            log.Fatalf("basic.publish: %v", err)
+            return false, err
+        }
 
-	if err != nil {
-		log.Fatalf("basic.publish: %v", err)
-		return false, err
-	}
-
-	return true, nil
+        return true, nil
+    };
 }
